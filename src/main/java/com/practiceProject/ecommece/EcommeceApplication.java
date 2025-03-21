befe5c2ee88087a57b662b2c6db3202304a71b7c
Muhammad Misbah <muhhhammadmisbah@gmail.com>
2025-03-21 21:44:10 +0500

    Code Refactored.

--- src/main/java/com/practiceProject/ecommece/EcommeceApplication.java
+++ src/main/java/com/practiceProject/ecommece/EcommeceApplication.java
@@ -6,11 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EcommeceApplication {
 
-	public static void main(String[] args) {
-		System.out.println("Here we go: ");
-		SpringApplication.run(EcommeceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        System.out.println("Here we go: ");
+        SpringApplication.run(EcommeceApplication.class, args);
+    }
 
 
 }
